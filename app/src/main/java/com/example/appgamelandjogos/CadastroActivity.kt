e71468dbea2373006c32b1e3c35ef9cf2bc046f4
unknown <3bmod@e041edu.local>
2026-04-30 22:48:10 -0300

    Tela de Cadastro

--- app/src/main/java/com/example/appgamelandjogos/CadastroActivity.kt
+++ app/src/main/java/com/example/appgamelandjogos/CadastroActivity.kt
@@ -1,27 +1,20 @@
 package com.example.appgamelandjogos
 
-import android.content.Intent
 import android.os.Bundle
-import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class MainActivity : AppCompatActivity() {
+class CadastroActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_cadastro)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
-        }
-        val btnCadastro = findViewById<Button>(R.id.buttoncadastrarlogin)
-        btnCadastro.setOnClickListener {
-            val intent = Intent( this, CadastroActivity::class.java)
-            startActivity(intent)
         }
     }
 }
